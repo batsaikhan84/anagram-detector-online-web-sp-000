222a538cbf5e50130a30a67e8f3f0e1b22fa47f4
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-21 20:29:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,18 +10,8 @@ class Anagram
     array.each do |element|
       array_element = element.split("").sort
       array_word = @word.split("").sort
-      w = 0
-      e = array_word.length - 1
-      while e < array_element.length
-        if array_element[w, e] == array_word[0, array_word.length - 1]
-          new_array << element
-          w += 1
-          e += 1
-        else
-          w += 1
-          e += 1
-        end
-      end
+      if array_element[0, array_word.length - 1] == array_word[0, array_word.length - 1]
+        new_array = []
     end
     new_array
   end
