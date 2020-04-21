6170685cb8a87a6ab2663ce11537dabefb117668
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-21 20:32:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
       array_element = element.split("").sort
       array_word = @word.split("").sort
       if array_element[0, array_word.length - 1] == array_word[0, array_word.length - 1]
-        new_array = []
+        new_array << element
       end
     end
     new_array
