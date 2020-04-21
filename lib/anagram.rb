30b5258d605ca9ea38263b43ec0f7fc956c0ecd9
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-21 19:19:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,6 +15,21 @@ array_1 = ["o", "r", "a", "n", "g", "e"]
 
 array_2 = ["a", "g", "e"]
 
+array_orange = "orange"
+array_age = "age"
 
+array_orange.split("")
+array_age.split("")
 
+n = 0
+m = array_age.length - 1
+
+while m < array_orange.length
+  if array_orange[n, m] == array_age[0, array_age.length - 1]
+    true
+  else
+    n += 1
+    m += 1
+  end
+end
 
