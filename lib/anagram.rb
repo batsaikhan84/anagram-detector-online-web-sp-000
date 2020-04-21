6e9f03c7619ff4231ae01112ef2e72037cf3efb3
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-21 19:09:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.each do |element|
+      
+  end
+end
+
+array_1 = ["o", "r", "a", "n", "g", "e"]
+
+array_2 = ["a", "g", "e"]
+
+
+puts array_1.sort
+puts array_2.sort
+
